0fe6b7c52a898cc967ab758e40b5ec13749a4bef
nikolakosturski <nkosturski6@gmail.com>
2024-11-07 10:47:40 +0100

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,384 @@
+﻿namespace Proektna_NikolaKosturski102424
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MatIzrazi = new System.Windows.Forms.TextBox();
+            this.Resenie = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.novaIgra = new System.Windows.Forms.Button();
+            this.potvrdi = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button10 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MatIzrazi
+            // 
+            this.MatIzrazi.Location = new System.Drawing.Point(23, 31);
+            this.MatIzrazi.Name = "MatIzrazi";
+            this.MatIzrazi.Size = new System.Drawing.Size(182, 23);
+            this.MatIzrazi.TabIndex = 0;
+            this.MatIzrazi.TextChanged += new System.EventHandler(this.MatIzrazi_TextChanged);
+            // 
+            // Resenie
+            // 
+            this.Resenie.Location = new System.Drawing.Point(23, 98);
+            this.Resenie.Name = "Resenie";
+            this.Resenie.Size = new System.Drawing.Size(182, 23);
+            this.Resenie.TabIndex = 1;
+            this.Resenie.Text = "Заокружете на една децимала";
+            this.Resenie.TextChanged += new System.EventHandler(this.Resenie_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(130, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(239, 167);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(23, 218);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(130, 218);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(239, 218);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(23, 267);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(130, 267);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(239, 267);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // novaIgra
+            // 
+            this.novaIgra.Location = new System.Drawing.Point(370, 30);
+            this.novaIgra.Name = "novaIgra";
+            this.novaIgra.Size = new System.Drawing.Size(134, 23);
+            this.novaIgra.TabIndex = 11;
+            this.novaIgra.Text = "Почни нова игра";
+            this.novaIgra.UseVisualStyleBackColor = true;
+            this.novaIgra.Click += new System.EventHandler(this.novaIgra_Click);
+            // 
+            // potvrdi
+            // 
+            this.potvrdi.Location = new System.Drawing.Point(239, 98);
+            this.potvrdi.Name = "potvrdi";
+            this.potvrdi.Size = new System.Drawing.Size(75, 23);
+            this.potvrdi.TabIndex = 12;
+            this.potvrdi.Text = "Потврди";
+            this.potvrdi.UseVisualStyleBackColor = true;
+            this.potvrdi.Click += new System.EventHandler(this.potvrdi_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(239, 308);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 13;
+            this.button12.Text = ".";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(130, 308);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 14;
+            this.button13.Text = "0";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(23, 308);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(75, 23);
+            this.button14.TabIndex = 15;
+            this.button14.Text = "<-";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(364, 132);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(212, 149);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Резултат";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(135, 38);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 15);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "3:00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(176, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(162, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Број на погрешни решенија:\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Број на точни решенија:\r\n";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Преостанато време:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 8);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(141, 15);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Реши го изразот подолу";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 77);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 15);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Решение";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(239, 138);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 19;
+            this.button10.Text = "-";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 350);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.potvrdi);
+            this.Controls.Add(this.novaIgra);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Resenie);
+            this.Controls.Add(this.MatIzrazi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox MatIzrazi;
+        private TextBox Resenie;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button novaIgra;
+        private Button potvrdi;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private GroupBox groupBox1;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Label label6;
+        private Label label7;
+        private System.Windows.Forms.Timer timer1;
+        private Label label8;
+        private Button button10;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Proektna_NikolaKosturski102424
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        string[] izrazi = new string[] {"3+5/3+4", "2+3*5+6","65+3/5",
+            "3*6+2/4","4*3+24/6","43*3-56/7","2+5*3","4/2+5","8+3*4","3-2*3",        //izrazi
+            "9/3+8*5","15/3+5*6","24/4+16*2","19-3+5*4","3*4-15/5",
+            "32/4+8*3","54/6+72/8","120/10+9*12","18/2+13*4","24+4/8","14*3-24/6",
+            "54+64/8","34+15-32/12","76-16/7","58/12+23","56+32-12*3","43+32/42/7",
+        "87+13/5","96+23/13+3","65+32"};
+        string[] resenija = new string[] {"8.6","23","65.6","18.5","16","121","21","7",   //odgovori
+            "20","-3","43","35","38","36","9","32","18","120","61","24.5","38","62",
+            "46.3","13.7","27.8","52","6.2","89.6","100.7","97" };
+        int x,counter1,counter2;
+        string y;
+        
+        
+         
+        public Form1()
+        {
+            InitializeComponent();
+            potvrdi.Enabled = false;
+        }
+
+        public string SetUpGame()
+        {
+            int index = rnd.Next(izrazi.Length);
+            MatIzrazi.Text = izrazi[index];
+            return MatIzrazi.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Resenie.Text += button1.Text;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Resenie.Text += button2.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Resenie.Text += button3.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Resenie.Text += button4.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Resenie.Text += button5.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Resenie.Text += button6.Text;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Resenie.Text += button7.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Resenie.Text += button8.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Resenie.Text += button9.Text;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Resenie.Text += button12.Text;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Resenie.Text += button13.Text;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            string brisi1, brisi2;
+            brisi1 = Resenie.Text;
+            brisi2 = brisi1.Remove(brisi1.Length - 1, 1);
+            Resenie.Text = brisi2;
+        }
+
+        private void MatIzrazi_TextChanged(object sender, EventArgs e)
+        {
+            MatIzrazi.ReadOnly= true;
+        }
+
+        private void Resenie_TextChanged(object sender, EventArgs e)
+        {
+            Resenie.Enabled = false;
+            
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+             Resenie.Text += button10.Text ;
+        }
+        private int s = 60; // sekundi
+        private int m = 2; //minuti
+
+        private void novaIgra_Click(object sender, EventArgs e) //kopce za nova igra
+        {
+            timer1.Enabled = false;
+            s = 60;
+            m = 2;
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Tick += new EventHandler(Tajmer);
+            timer1.Interval = 1000;
+            timer1.Start();
+            counter1 = 0;
+            label4.Text = counter1.ToString();
+            counter2 = 0;
+            label5.Text = counter2.ToString();
+            Resenie.Text = string.Empty;
+            SetUpGame();
+            potvrdi.Enabled = true;
+  
+        }
+
+        private void Tajmer(object sender, EventArgs e)  // funkcija za tajmerot
+        {
+            if (s >= 0 && m > 0)
+            {
+                s--;
+                if (s == 0)
+                {
+                    m--;
+
+                    s = 60;
+                }
+
+                label8.Text = m + ":" + s.ToString();
+            }
+            else if (s > 0)
+
+            {
+                s--;
+                label8.Text = m + ":" + s;
+            }
+            else if (s == 0 && m == 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Крај на играта");
+
+            }
+        }
+
+        private void potvrdi_Click(object sender, EventArgs e) //kopce potvrdi
+        {
+            if(izrazi.Contains(MatIzrazi.Text))
+            {
+                x = Array.LastIndexOf(izrazi, MatIzrazi.Text);
+                y = resenija[x];
+
+                if(string.Equals(Resenie.Text,y))
+                {
+                    counter1++;
+                    label4.Text = counter1.ToString();
+                    Resenie.Text = string.Empty;
+                    SetUpGame();
+                }
+                else
+                {
+                    counter2++;
+                    label5.Text = counter2.ToString();
+                    Resenie.Text = String.Empty;
+                    SetUpGame();
+                }
+            }
+        }
+    }
+}
